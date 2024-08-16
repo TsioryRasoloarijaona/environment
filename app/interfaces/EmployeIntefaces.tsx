b0d0f8241c3c4ad37c7ddf4ad535b87a5f52de40
Tsiory <hei.tsiory@gmail.com>
2024-08-17 00:13:36 +0300

    feat: all employes data

---
+++ app/interfaces/EmployeIntefaces.tsx
@@ -0,0 +1,9 @@
+export interface Employee {
+    id: string;
+    name: string;
+    email: string;
+    image: string;
+    role: string;
+    telephone: string;
+  }
+  
---
+++ app/interfaces/userInterface.tsx
@@ -0,0 +1,9 @@
+export interface User {
+    userId: string;
+    email: string;
+    name: string;
+    telephone: string;
+    image: string;
+    plantCount: number;
+}
+  
