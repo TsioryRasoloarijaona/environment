cba1116712dde8b104916e7cc65926d70982aba8
Ranto-Creat <hei.ranto.2@gmail.com>
2024-08-16 13:25:58 +0300

    fix: add the section footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,12 @@
 import Image from "next/image";
 import CardComponent from "./components/Card";
+import Footer from "./components/Footer/Footer";
+
 
 export default function Home() {
   return (
     <main className="bg-gray-primary">
-      <div className="grid grid-cols-3 w-[80vw] mx-auto py-16">
-        <div className="mx-auto">
-          <CardComponent />
-        </div>
-        <div className="mx-auto">
-          <CardComponent />
-        </div>
-        <div className="mx-auto ">
-          <CardComponent />
-        </div>
-        <div className="mx-auto">
-          <CardComponent />
-        </div>
-        <div className="mx-auto">
-          <CardComponent />
-        </div>
-        <div className="mx-auto">
-          <CardComponent />
-        </div>
-        
-       
-      </div>
+      <Footer/>
     </main>
   );
 }
